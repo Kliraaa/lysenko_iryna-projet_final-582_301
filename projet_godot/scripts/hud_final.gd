7cdccff4f966abac5a340aadca24a0597875b3f2
Kliraaa <2472632@cmontmorency.qc.ca>
2025-12-14 22:06:15 -0500

    tried to do light bar, found sfx for running and light pickup

--- projet_godot/scripts/hud_final.gd
+++ projet_godot/scripts/hud_final.gd
@@ -5,12 +5,19 @@ var hearts_list = []
 @onready var key = $KeyHUD
 var hasKey := false
 
+@onready var stamina = $light_bar
+var s_timer = 0
+var can_start_stimer = true
+
+
+
 func _ready():
 	var hearts_parent = $HBoxContainer
 	for child in hearts_parent.get_children():
 		hearts_list.append(child)
 
 	update_hearts(health)
+	stamina.value = stamina.min_value
 
 func update_hearts(value):
 	health = value
